86c9828cc9926a7aa17ebd377306510eb0ca09b2
Tezaphon <maxzosenko@gmail.com>
2026-09-03 14:02:55 +0200

    Added: readme.txt, general functionality. Issues: end quiz loop, not counting the points

--- oppgave_torsdag.py
+++ oppgave_torsdag.py
@@ -1,7 +1,9 @@
 import random
+from colorama import Fore
 
-points=0
-
+pointsGlobal=0
+list=[]
+#class definition
 class QA:
     def __init__(self, question, answer, pointsReward):
         self.question=question
@@ -9,16 +11,56 @@ class QA:
         self.pointsReward=pointsReward
         self.done=False
     def showQuestion(self):
+        print("------------------------------------")
         print(self.question)
     def checkAnswer(self, answer):
+        self.done=True
         if answer == self.answer:
-            points+=1
+            print(Fore.GREEN+"Riktig!"+Fore.RESET)
+            return self.pointsReward
+        else:
+            print(Fore.RED+"Feil!"+Fore.RESET)
+            return 0
 
-list=[]
-list.extend([QA("Hva er hovedstaden i Frankrike?", "Paris", 1),QA("Hvor mange dager er det i året?","365",1),QA("Hor mange planeter er det i solsystemet","8",1)])
+#list definition
+#can add new questions by following a blueprint under
+#QA("question", "answer", points reward)
 
-def ask():
+list.extend([
+    QA("Hva er hovedstaden i Frankrike?", "Paris", 1),
+    QA("Hvor mange dager er det i året?","365",1),
+    QA("Hvor mange planeter er det i solsystemet","8",1),
+    QA("Hvor mange måner Mars har?","2",2),
+    QA("Hvor høyt er Galdhøppingen?","2469",4),
+    QA("Hvilket år fikk Norge grunnlov?","1814",3)
+    ])
+
+#quiz functional parts definition
+
+quizRunning=True
+def checkQuiz():
+    _notAnsweredQuestionsCounter=0
     for i in list:
-        i.showQuestion()
+        if i.done==False:
+            _notAnsweredQuestionsCounter+=1
+    if _notAnsweredQuestionsCounter ==0:
+        quizRunning=False
+        print("====================================")
+        print("Quizen er over.")
+        print("Poeng - "+str(pointsGlobal))
+        print("====================================")
 
-ask()
+def ask(_points):
+    index=random.randint(0, len(list)-1)
+    if list[index].done==False:
+        list[index].showQuestion()
+        svar=input("Ditt svar: ")
+        _points+=list[index].checkAnswer(svar)
+
+#runtime
+
+print("Velkommen til en quiz")
+start=input("Start?")
+while quizRunning==True:
+    ask(pointsGlobal)
+    checkQuiz()
---
+++ readme.txt
@@ -0,0 +1,1 @@
+do "pip install colorama" in cmd
